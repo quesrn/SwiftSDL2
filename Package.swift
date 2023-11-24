138eee3979b0bd020675861c0316d08a8d31e4a4
Alex Ehlke <alex.ehlke@gmail.com>
2023-11-24 15:40:18 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "SDL",
     platforms: [
-        .macOS(.v12),
-        .iOS(.v15),
+        .macOS(.v13),
+        .iOS(.v16),
 //        .tvOS(.v13),
     ],
     products: [
@@ -15,24 +15,24 @@ let package = Package(
         .target(name: "SDL",
                 dependencies: [
                     .target(name: "SDL2", condition: .when(platforms: [.macOS, .iOS])), //, .tvOS])),
-                    .target(name: "CSDL2", condition: .when(platforms: [.linux, .windows])),
+//                    .target(name: "CSDL2", condition: .when(platforms: [.linux, .windows])),
                 ],
                 path: "Sources/SDL2"),
-        .testTarget(name: "SDLTests", dependencies: ["SDL"]),
+//        .testTarget(name: "SDLTests", dependencies: ["SDL"]),
         .binaryTarget(name: "SDL2", path: "SDL2.xcframework"),
-        .systemLibrary(
-            name: "CSDL2",
-            pkgConfig: "sdl2",
-            providers: [
-                .apt(["libsdl2-dev"]),
-            ]
-        ),
-        .executableTarget(name: "Minimal", dependencies: ["SDL"], path: "Sources/Demos/Minimal"),
-        .executableTarget(
-            name: "MetalApp",
-            dependencies: ["SDL"],
-            path: "Sources/Demos/MetalApp",
-            swiftSettings: [.define("METAL_ENABLED", .when(platforms: [.macOS]))]
-        ),
+//        .systemLibrary(
+//            name: "CSDL2",
+//            pkgConfig: "sdl2",
+//            providers: [
+//                .apt(["libsdl2-dev"]),
+//            ]
+//        ),
+//        .executableTarget(name: "Minimal", dependencies: ["SDL"], path: "Sources/Demos/Minimal"),
+//        .executableTarget(
+//            name: "MetalApp",
+//            dependencies: ["SDL"],
+//            path: "Sources/Demos/MetalApp",
+//            swiftSettings: [.define("METAL_ENABLED", .when(platforms: [.macOS]))]
+//        ),
     ]
 )
